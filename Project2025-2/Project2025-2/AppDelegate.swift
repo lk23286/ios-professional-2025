a89551f577e6683804a8a439956f96465a7e1ec8
Laszlo Kovacs <lk23286@gmailcom>
2025-03-01 20:55:35 +0100

    Project2025-2 OnboardingContainerViewController

--- Project2025-2/Project2025-2/AppDelegate.swift
+++ Project2025-2/Project2025-2/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = LoginViewController()
-       // window?.rootViewController = OnboardingContainerViewController()
+        // window?.rootViewController = LoginViewController()
+        window?.rootViewController = OnboardingContainerViewController()
        // window?.rootViewController = OnboardingViewContainer(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989.")
         
         return true
